bd869c18ba1afb2e846e526d3828eb75e064e5a4
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-06-26 13:28:25 +0400

    Update!

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const { Telegram, InlineKeyboard } = require("puregram");
 const translations = require("./translations")
 const md5 = require("md5")
 const RC4 = require("simple-rc4")
+const fetch = require("node-fetch")
+const cheerio = require("cheerio")
 
 let MESSAGES_DB = []
 let MESSAGES_TEMP_HANDLER = []
+let CACHED_TAGS = []
 
 const telegram = Telegram.fromToken(process.env.TOKEN)
 
@@ -25,7 +28,7 @@ telegram.updates.on('inline_query', async(ctx) => {
     let args = ctx.query.split(" ").filter(e => e != '')
     let message = translation.botMeeting
     let TEMP_ID = md5(Date.now())
-    let userTag = ""
+    let userTag = []
     let messageTypen = false
 
     let query = [
@@ -43,31 +46,82 @@ telegram.updates.on('inline_query', async(ctx) => {
     const errorMessages = {
         0: translation.onlyOneArg,
         1: translation.incorrectTag,
-        2: translation.sendingToBot
+        2: translation.sendingToBot,
+        3: translation.moreThan10
     }
 
     if(args.length == 1){
-        args[0] = args[0].replaceAll(/^(@|!)/gi, "")
-        let errors = /^(?!_)([a-zA-Z0-9_]{5,32})(?<!_)$/.test(args[0]) ? 0 : 
-        (args[0].endsWith("bot") ? 2 : 1)
+        args[0] = args[0].replaceAll(/^(@|!)/gi, "").toLowerCase().split(",")
 
-        message = errorMessages[errors]
+        let errors = args[0].length > 10 ? 3 : 0,
+            errorReason = ""
+            
+        message = errorMessages[0]
+
+        for(let tag of args[0]){
+            errors = /^(?!_)([a-zA-Z0-9_]{5,32})(?<!_)$/.test(tag) ? 
+            (tag.endsWith("bot") ? 2 : 0) : 1
+            
+            if(errors != 0){
+                errorReason = tag
+                message = errorMessages[errors]
+                break;
+            }else{
+                if(!CACHED_TAGS.includes(tag)){
+                    const checkForTag = await checkTag(tag)
+
+                    if(checkForTag){
+                        CACHED_TAGS.push(tag)
+                    }else{
+                        message = errorMessages[1]
+                        break;
+                    }
+                }
+            }
+        }
+
         query = []
     } else if(args.length > 1) {
         let readableAgain = !args[0].startsWith("!")
-        userTag = args[0].replaceAll(/^(@|!)/gi, "")
+        userTag = args[0].replaceAll(/^(@|!)/gi, "").toLowerCase().split(",")
 
-        let errors = /^(?!_)([a-zA-Z0-9_]{5,32})(?<!_)$/.test(userTag) ? 0 : 
-        (userTag.endsWith("bot") ? 2 : 1)
+        let errors = userTag.length > 10 ? 3 : 0,
+            errorReason = ""
+
+            for(let tag of args[0]){
+                errors = /^(?!_)([a-zA-Z0-9_]{5,32})(?<!_)$/.test(tag) ? 
+                (tag.endsWith("bot") ? 2 : 0) : 1
+                
+                if(errors != 0){
+                    errorReason = tag
+                    message = errorMessages[errors]
+                    break;
+                }else{
+                    if(!CACHED_TAGS.includes(tag)){
+                        const checkForTag = await checkTag(tag)
+    
+                        if(checkForTag){
+                            CACHED_TAGS.push(tag)
+                        }else{
+                            message = errorMessages[1]
+                            break;
+                        }
+                    }
+                }
+            }
+
+        args = args.slice(1).join(" ")
 
         if(errors == 0){
-            const CRYPTO = new RC4(Buffer.from(userTag + ctx.from.id))
-            
-            args = args.slice(1).join(" ")
+            let messages = []
+            for(let user of userTag){
+                const CRYPTO = new RC4(Buffer.from(user.toLowerCase() + ctx.from.id))
+                
+                let cryptedMessage = Buffer.from(args)
+                CRYPTO.update(cryptedMessage)
 
-            let cryptedMessage = Buffer.from(args)
-            CRYPTO.update(cryptedMessage)
-
+                messages.push(cryptedMessage)
+            }
             message = translation.sent
             TEMP_ID = md5(userTag + ctx.from.id + Date.now()) 
 
@@ -77,7 +131,7 @@ telegram.updates.on('inline_query', async(ctx) => {
 
             MESSAGES_TEMP_HANDLER.push({
                 id: TEMP_ID,
-                message: cryptedMessage,
+                messages,
                 messageHash: md5(args),
                 readableAgain: readableAgain,
                 sender: ctx.from.id,
@@ -92,21 +146,33 @@ telegram.updates.on('inline_query', async(ctx) => {
         query = []
     }
 
-    const keyboard = InlineKeyboard.keyboard([
+    let keyboard = InlineKeyboard.keyboard([
         InlineKeyboard.textButton({
             text: translation.read,
             payload: {
+                action: "read",
+                id: TEMP_ID
+            }
+        }),
+        InlineKeyboard.textButton({
+            text: translation.delete,
+            payload: {
+                action: "delete",
                 id: TEMP_ID
             }
         })
     ])
+
+    /*if(userTag.length > 1){
+        keyboard = keyboard.slice(0, -1) // because i'm lazy to do reading
+    }*/
 
     query.push(
         { 
             type: "article", 
             title: message.title, 
             input_message_content: { 
-                message_text: message.message_text.replace("{tag}", userTag).replace("{sender}", ctx.from.firstName),
+                message_text: message.message_text.replace("{tags}", userTag.map(e => `@${e}`).join(", ")).replace("{sender}", ctx.from.firstName),
             }, 
             reply_markup: messageTypen ? keyboard : undefined,
             id: TEMP_ID, 
@@ -114,7 +180,7 @@ telegram.updates.on('inline_query', async(ctx) => {
         }
     )
 
-    ctx.answerInlineQuery(query, {
+    await ctx.answerInlineQuery(query, {
         cache_time: 1
     })
 })
@@ -143,18 +209,38 @@ telegram.updates.on('callback_query', async(ctx) => {
             show_alert: true
         })
     }
+
+    let hashPositive = false,
+        uncryptedMessage = ""
+
+    for(let message of MESSAGE.messages){
+        const CRYPTO = new RC4(Buffer.from(ctx.from.username.toLowerCase() + MESSAGE.sender))
+
+        uncryptedMessage = Buffer.from(message)
+
+        CRYPTO.update(uncryptedMessage)
+
+        uncryptedMessage = uncryptedMessage.toString("utf8")
+
+        if(MESSAGE.messageHash == md5(uncryptedMessage)){
+            hashPositive = true
+            break;
+        }
+    }
     
-    const CRYPTO = new RC4(Buffer.from(ctx.from.username + MESSAGE.sender))
-    let uncryptedMessage = Buffer.from(MESSAGE.message)
-
-    CRYPTO.update(uncryptedMessage)
-
-    uncryptedMessage = uncryptedMessage.toString("utf8")
-
-    if(MESSAGE.messageHash != md5(uncryptedMessage))return ctx.answerCallbackQuery({
+    if(!hashPositive)return ctx.answerCallbackQuery({
         text: translation.notForYou,
         show_alert: true
     })
+
+    if(ctx.queryPayload.action == "delete"){
+        MESSAGES_DB = MESSAGES_DB.filter(e => e.id != MESSAGE.id)
+    
+        return ctx.answerCallbackQuery({
+            text: translation.deleted,
+            show_alert: true
+        })
+    }
 
     if(!MESSAGE.readableAgain){
         MESSAGES_DB = MESSAGES_DB.filter(e => e.id != MESSAGE.id)
@@ -202,4 +288,11 @@ process.on("unhandledRejection", e => {
 
 function localizeJSON(object){
     return JSON.parse(JSON.stringify(object))
+}
+
+async function checkTag(tag){
+    const result = (await (await fetch("https://t.me/" + tag)).text())
+    const $ = cheerio.load(result)
+
+    return $("div.tgme_page_extra")[0] ? $("div.tgme_page_extra")[0].children[0].data.trim().startsWith("@") : false
 }
