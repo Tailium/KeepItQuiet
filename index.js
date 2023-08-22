cdf2d4a6456c2a5aea7c738b335158db59ef803b
тейлс <49571184+tailsjs@users.noreply.github.com>
2023-08-23 03:20:43 +0400

    v1.2.1

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ dotenv.config()
 const { Telegram, InlineKeyboard } = require("puregram");
 const translations = require("./translations")
 const md5 = require("md5")
+const sha1 = require("js-sha1")
 const RC4 = require("simple-rc4")
 const fetch = require("node-fetch")
 const cheerio = require("cheerio")
@@ -26,7 +27,7 @@ telegram.updates.on('inline_query', async(ctx) => {
     const translation = translations[Object.keys(translations).includes(ctx.from.languageCode) ? ctx.from.languageCode : "en"]
     let args = ctx.query.split(" ").filter(e => e != '')
     let message = translation.botMeeting
-    let TEMP_ID = md5(Date.now())
+    let TEMP_ID = hash(Date.now())
     let userTag = []
     let messageTypen = false
     let readableAgain = true
@@ -53,67 +54,32 @@ telegram.updates.on('inline_query', async(ctx) => {
     if(args.length == 1){
         args[0] = args[0].replaceAll(/(@|!)/gi, "").toLowerCase().split(",")
 
-        let errors = args[0].length > 10 ? 3 : 0,
-            errorReason = ""
+        let errorMessage = args[0].length > 10 ? 3 : 0
             
         message = errorMessages[0]
 
         for(let tag of args[0]){
-            errors = /^(?!_)([a-zA-Z0-9_]{5,32})(?<!_)$/.test(tag) ? 
-            (tag.endsWith("bot") ? 2 : 0) : 1
+            errorMessage = await isTagValid(tag)
 
-            if(errors != 0){
-                errorReason = tag
-                message = errorMessages[errors]
-                break;
-            }else{
-                if(!CACHED_TAGS.includes(tag)){
-                    const checkForTag = await checkTag(tag)
-                    
-                    if(checkForTag){
-                        CACHED_TAGS.push(tag)
-                    }else{
-                        message = errorMessages[1]
-                        break;
-                    }
-                }
-            }
+            message = errorMessages[errorMessage]
         }
 
         query = []
-    } else if(args.length > 1) { // todo: make tagChecking one-functional
+    } else if(args.length > 1) {
         readableAgain = !args[0].startsWith("!")
         userTag = args[0].replaceAll(/(@|!)/gi, "").toLowerCase().split(",")
 
-        let errors = userTag.length > 10 ? 3 : 0,
-            errorReason = ""
+        let errorMessage = userTag.length > 10 ? 3 : 0
 
-            for(let tag of userTag){
-                errors = /^(?!_)([a-zA-Z0-9_]{5,32})(?<!_)$/.test(tag) ? 
-                (tag.endsWith("bot") ? 2 : 0) : 1
+        for(let tag of userTag){
+            errorMessage = await isTagValid(tag)
 
-                if(errors != 0){
-                    errorReason = tag
-                    message = errorMessages[errors]
-                    break;
-                }else{
-                    if(!CACHED_TAGS.includes(tag)){
-                        const checkForTag = await checkTag(tag)
-
-                        if(checkForTag){
-                            CACHED_TAGS.push(tag)
-                        }else{
-                            message = errorMessages[1]
-                            errors = 1
-                            break;
-                        }
-                    }
-                }
-            }
+            message = errorMessages[errorMessage]
+        }
 
         args = args.slice(1).join(" ")
 
-        if(errors == 0){
+        if(errorMessage == 0){
             let messages = []
             for(let user of userTag){
                 const CRYPTO = new RC4(Buffer.from(user.toLowerCase() + ctx.from.id))
@@ -133,16 +99,17 @@ telegram.updates.on('inline_query', async(ctx) => {
             MESSAGES_TEMP_HANDLER.push({
                 id: TEMP_ID,
                 messages,
-                messageHash: md5(args),
+                messageHash: hash(args),
                 readableAgain: readableAgain,
                 sender: ctx.from.id,
                 readers: [],
-                sent: Date.now()
+                sent: Date.now(),
+                expires: Date.now() + 864e5
             })
 
             messageTypen = true
         }else{
-            message = errorMessages[errors]
+            message = errorMessages[errorMessage]
         }
         query = []
     }
@@ -239,15 +206,6 @@ telegram.updates.on('callback_query', async(ctx) => {
         show_alert: false
     })
 
-    if(Date.now()-MESSAGE.sent > 864e5) {
-        MESSAGES_DB = MESSAGES_DB.filter(e => e.id != MESSAGE.id)
-
-        return ctx.answerCallbackQuery({
-            text: translation.outdated,
-            show_alert: false
-        })
-    }
-
     if(ctx.queryPayload.action == "check"){
         if(ctx.from.id == MESSAGE.sender){
             return ctx.answerCallbackQuery({
@@ -275,8 +233,8 @@ telegram.updates.on('callback_query', async(ctx) => {
 
         uncryptedMessage = uncryptedMessage.toString("utf8")
 
-        if(MESSAGE.messageHash == md5(uncryptedMessage)){
-            messageHash = md5(Buffer.from(message).toString("utf8"))
+        if(MESSAGE.messageHash == hash(uncryptedMessage)){
+            messageHash = hash(Buffer.from(message).toString("utf8"))
             hashPositive = true
             break;
         }
@@ -340,7 +298,10 @@ telegram.updates.on('callback_query', async(ctx) => {
 })
 
 telegram.updates.startPolling()
-  .then(() => console.log(`Keeping quiet at @${telegram.bot.username}!`))
+  .then(() => {
+    console.log(`Keeping quiet at @${telegram.bot.username}!`)
+    setInterval(clearBases, 10000)
+  })
   .catch(console.error)
 
 process.on("uncaughtException", e => {
@@ -362,6 +323,37 @@ async function checkTag(tag){
     return $("div.tgme_page_extra")[0] ? $("div.tgme_page_extra")[0].children[0].data.trim().startsWith("@") : false
 }
 
-async function isTagValid(tag){
-    // in next update
+async function isTagValid(tag) {
+    let errors = /^(?!_)([a-zA-Z0-9_]{5,32})(?<!_)$/.test(tag) ?
+        (tag.endsWith("bot") ? 2 : 0) : 1
+
+    if (errors != 0) {
+        return errors
+    } else {
+        if (!CACHED_TAGS.find(e => e.tag == hash(tag))) {
+            const checkForTag = await checkTag(tag)
+
+            if (checkForTag) {
+                CACHED_TAGS.push({
+                    tag: hash(tag),
+                    expire: Date.now() + 43200000
+                })
+                return 0
+            } else {
+                return 1
+            }
+        }else{
+            return 0
+        }
+    }
+}
+
+function hash(text){
+    return sha1(typeof text != "string" ? String(text) : text)
+}
+
+function clearBases(){
+    // Tags
+    CACHED_TAGS = CACHED_TAGS.filter(e => Date.now() < e.expire)
+    MESSAGES_DB = MESSAGES_DB.filter(e => Date.now() < e.expires)
 }
